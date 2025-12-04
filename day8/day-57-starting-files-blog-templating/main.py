b34296867c47603564afebc2990ef5e9ecc449d5
Syed Alauddin Bukhari <syedalauddin.b@gmail.com>
2025-12-03 22:05:55 -0700

    UPDATE day8

--- day8/day-57-starting-files-blog-templating/main.py
+++ day8/day-57-starting-files-blog-templating/main.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
-
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    url = "https://www.npoint.io/docs/c790b4d5cab58020d391"
+    
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        posts = response.json()
+    except requests.exceptions.RequestException as e:
+        print("Error fetching blog data:", e)
+        posts = []  # fallback
+
+    return render_template("index.html", posts=posts)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
